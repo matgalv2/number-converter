00552bfbda6727c6f651ec540b4ef6d5bd1a9191
Mateusz <matgalv2@gmail.com>
2023-02-11 13:42:12 +0100

    Dependency update

--- src/main/java/io/github/matgalv2/numberconverter/controller/NumberConverterController.java
+++ src/main/java/io/github/matgalv2/numberconverter/controller/NumberConverterController.java
@@ -2,12 +2,12 @@ package io.github.matgalv2.numberconverter.controller;
 
 import io.github.matgalv2.numberconverter.dto.Data;
 import io.github.matgalv2.numberconverter.serivice.NumberConverterService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import javax.validation.Valid;
 
 
 @Controller
