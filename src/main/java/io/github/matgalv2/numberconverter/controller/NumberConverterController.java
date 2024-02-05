d361503bdaa42401e151f83f65d2ccb952210ff5
Mateusz <matgalv2@gmail.com>
2024-02-05 23:46:52 +0100

    Changed GET for POST

--- src/main/java/io/github/matgalv2/numberconverter/controller/NumberConverterController.java
+++ src/main/java/io/github/matgalv2/numberconverter/controller/NumberConverterController.java
@@ -5,7 +5,7 @@ import io.github.matgalv2.numberconverter.service.NumberConverterService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
@@ -17,7 +17,7 @@ public class NumberConverterController {
 
     private final NumberConverterService numberConverterService;
 
-    @GetMapping("/convert")
+    @PostMapping("/convert")
     public ResponseEntity<?> getConvertedNumber(@Valid @RequestBody Data data) {
         return numberConverterService
                 .convertNumber(data)
